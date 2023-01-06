a6f6b9f61e8cc889d013b20697ca08fdfdde0805
Ryan Izac <io.ryan@outlook.com>
2023-01-06 17:12:57 -0300

    conf: add prettier and jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  roots: ["<rootDir>/src"],
+  testEnvironment: "node",
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+  },
+};
